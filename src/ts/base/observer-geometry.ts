a5adb4df6e36365cf64cae92490366ee404e4bb2
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-12-12 02:58:01 +0200

    made main-section, his adaptations and styles, created logic for animation

---
+++ src/ts/base/observer-geometry.ts
@@ -0,0 +1,38 @@
+export function observerGeometry(element: HTMLElement, duration: number, scaleTo: number): void {
+    const observer: IntersectionObserver = new IntersectionObserver(
+        (entries: IntersectionObserverEntry[]): void => {
+            entries.forEach((entry: IntersectionObserverEntry): void => {
+                if (entry.isIntersecting) {
+                    animationScale(element, duration, scaleTo);
+                }
+            });
+        },
+        {
+            threshold: 0.1,
+        },
+    );
+
+    observer.observe(element);
+}
+
+function animationScale(element: HTMLElement, duration: number, scaleTo: number): void {
+    const startTime: number = performance.now();
+    const initialScale: number = 1;
+
+    function step(currentTime: number): void {
+        const elapsedTime = currentTime - startTime;
+        const progress: number = Math.min(elapsedTime / duration, 1);
+        const scale = initialScale + (scaleTo - initialScale) * easeInOutQuad(progress);
+        element.style.transform = `scale(${scale})`;
+
+        if (progress < 1) {
+            requestAnimationFrame(step);
+        }
+    }
+
+    requestAnimationFrame(step);
+}
+
+function easeInOutQuad(t: number): number {
+    return t < 0.5 ? 2 * t * t : 1 - Math.pow(-2 * t + 2, 2) / 2;
+}
--- src/ts/main-page.ts
+++ src/ts/main-page.ts
@@ -1,6 +1,17 @@
 import { burgerMenu, burgerSubMenu } from "./base/burger-menu.ts";
+import { observerGeometry } from "./base/observer-geometry.ts";
+import { getElement } from "./base/get-element.ts";
 
 document.addEventListener("DOMContentLoaded", async function (): Promise<void> {
     burgerMenu();
     burgerSubMenu();
+    observerGeometry(getElement(".main-section__circle-purple") as HTMLElement, 2700, 105);
+
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth === 375) {
+        observerGeometry(getElement(".main-section__circle-yellow") as HTMLElement, 2500, 59);
+    } else {
+        observerGeometry(getElement(".main-section__circle-yellow") as HTMLElement, 2500, 95);
+    }
 });
