960694cc069b98c5c6408332fdc92f4b2382e80c
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-12-19 18:30:18 +0200

    add swiper

--- src/ts/base/swiper.ts
+++ src/ts/base/swiper.ts
@@ -1,5 +1,5 @@
 import Swiper from "swiper";
-import "swiper/css";
+// import "swiper/css";
 
 let swiperInstance: Swiper | null = null;
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,25 @@ import injectHTML from "vite-plugin-html-inject";
 import { resolve } from "path";
 
 export default defineConfig({
-    plugins: [[injectHTML()]],
-    base: "/vitamin-online/",
-    server: {
-        open: true,
-    },
-    build: {
-        outDir: "dist",
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"),
-                404: resolve(__dirname, "404.html"),
-            },
-        },
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                quietDeps: true,
-            },
-        },
-    },
+	plugins: [[injectHTML()]],
+	base: "/vitamin-online/",
+	server: {
+		open: true,
+	},
+	build: {
+		outDir: "dist",
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				404: resolve(__dirname, "404-section.html"),
+			},
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true,
+			},
+		},
+	},
 });
